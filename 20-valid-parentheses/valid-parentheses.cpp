731a4357a00b1d346fa51b165421195deac5f8fa
Kavya980 <kavyaa17jk@gmail.com>
2026-02-08 18:18:54 +0530

    Time: 3 ms (9.43%) | Memory: 8.9 MB (66.59%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-   for(char c:s){
-    if(c=='('||c=='{'||c=='['){
-        st.push(c);
-    }else{
-      if(st.empty()){
-        return false;}
-        char top=st.top();
-        st.pop();
-         if((c == ')' && top != '(') ||
-                   (c == '}' && top != '{') ||
-                   (c == ']' && top != '[')) {
+        for(char c:s){
+            if(c=='('||c=='{'||c=='['){
+                st.push(c);
+            }else{
+                if(st.empty()){
                     return false;
-      }
-    }
-   }return st.empty();
+                    }     
+                    char top=st.top();
+                    st.pop();
+                    if((c==')' && top!='(') ||(c=='}' && top!='{') ||(c==']' && top!='[' )){
+                        return false;
+                    }
+                 }
+        }
+        return st.empty();
     }
 };
