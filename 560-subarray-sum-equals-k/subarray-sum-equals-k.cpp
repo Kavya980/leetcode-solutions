b6f0eda3afe80cc0cf2f9cc11c0f45d8929e4bce
Kavya980 <kavyaa17jk@gmail.com>
2026-03-28 00:17:22 +0530

    Time: 39 ms (72.02%) | Memory: 45.4 MB (69.47%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,14 +5,16 @@ public:
         mp[0]=1;      
         int sum=0;
         int count=0;
-        for (int i=0;i<nums.size();i++) {
-            sum+=nums[i];
-            if(mp.find(sum-k)!=mp.end()) {
+        for (int num:nums) {
+            sum+=num;
+            if(mp.count(sum-k)) {
                 count+=mp[sum-k];
             }
+            //  if(mp.find(sum-k)!=mp.end()) {
+            //     count+=mp[sum-k];
+            // }
             mp[sum]++;
         }
-
         return count;
     } 
 };
