c3bce33880c9a00a88d97159e37d3754bbfb4917
Kavya980 <kavyaa17jk@gmail.com>
2026-04-02 01:29:38 +0530

    Time: 3 ms (41.13%) | Memory: 12.8 MB (35.75%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
-        for(int i=0;i<nums1.size();i++){
-             bool isGreater=false;
-
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                   
-                    for(int k=j+1;k<nums2.size();k++){
-                    if(nums2[j]<nums2[k]){
-                        isGreater=true;
-                        result.push_back(nums2[k]);
-                        break;
-                    }}if(!isGreater){
-                        result.push_back(-1);
-                    }
-                    break;
-                }
-            }
+       stack<int> st;
+       unordered_map<int,int>mp;
+       for(int num:nums2){
+        while(!st.empty() && num>st.top()){
+            mp[st.top()]=num;
+            st.pop();
         }
-        return result;
+        st.push(num);
+       }
+       vector<int> ans;
+       for(int num:nums1){
+        if(mp.find(num)!=mp.end()){   //find key,end means not found
+            ans.push_back(mp[num]);
+        }else
+        ans.push_back(-1);
+       }
+       return ans;
     }
 };
+
+//  vector<int> result;
+//         for(int i=0;i<nums1.size();i++){
+//              bool isGreater=false;
+
+//             for(int j=0;j<nums2.size();j++){
+//                 if(nums1[i]==nums2[j]){
+                   
+//                     for(int k=j+1;k<nums2.size();k++){
+//                     if(nums2[j]<nums2[k]){
+//                         isGreater=true;
+//                         result.push_back(nums2[k]);
+//                         break;
+//                     }}if(!isGreater){
+//                         result.push_back(-1);
+//                     }
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
