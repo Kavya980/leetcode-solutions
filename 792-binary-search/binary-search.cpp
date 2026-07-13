858a040adc32fadf1db8bcec43ec67470180bb6a
Kavya <kavyaa17jk@gmail.com>
2026-07-13 17:52:55 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (99.17%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -6,7 +6,7 @@ public:
         int end=n-1;
         
         while(beg<=end){
-            int mid=(beg+end)/2;
+            int mid=beg+(end-beg)/2;
             if(nums[mid]==target)
                 return mid;
             else if(nums[mid]>target)
